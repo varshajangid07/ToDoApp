ed3fd920cef807d9b8be39fd4af1267fcbffca7c
Christoph Ernst <chernst@viona.rocks>
2026-07-20 12:22:00 +0200

    generated header component, implement header teaser, implement logo, added responsive design to header component, fixes #1, fixes #2, fixes #3, fixes #4

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header-component/header-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
